4022c5bef997765398cadf2200fdc0e7cbdff380
Ravinath Akalanka <rajaravinath0@gmail.com>
2025-12-16 19:46:39 +0530

    Implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,38 @@
 import { useGSAP } from '@gsap/react';
 import { SplitText } from 'gsap/all';
 import gsap from 'gsap';
+import { useRef } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
-    const heroSplit = new SplitText('.title', { type: 'chars, words' });
-    const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+    document.fonts.ready.then(() => {
+      const heroSplit = new SplitText('.title', { type: 'chars, words' });
+      const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
 
-    heroSplit.chars.forEach((char) => {
-      char.classList.add('text-gradient');
-    });
+      heroSplit.chars.forEach((char) => {
+        char.classList.add('text-gradient');
+      });
 
-    gsap.from(heroSplit.chars, {
-      yPercent: 100,
-      duration: 1.8,
-      ease: 'expo.out',
-      stagger: 0.05,
-    });
+      gsap.from(heroSplit.chars, {
+        yPercent: 100,
+        duration: 1.8,
+        ease: 'expo.out',
+        stagger: 0.05,
+      });
 
-    gsap.from(paragraphSplit.lines, {
-      opacity: 0,
-      yPercent: 100,
-      duration: 1.8,
-      ease: 'expo.out',
-      stagger: 0.05,
-      delay: 1,
+      gsap.from(paragraphSplit.lines, {
+        opacity: 0,
+        yPercent: 100,
+        duration: 1.8,
+        ease: 'expo.out',
+        stagger: 0.05,
+        delay: 1,
+      });
     });
 
     gsap
@@ -38,6 +46,25 @@ const Hero = () => {
       })
       .to('.right-leaf', { y: 200 }, 0)
       .to('.left-leaf', { y: -200 }, 0);
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
@@ -68,6 +95,10 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className='video absolute inset-0'>
+        <video ref={videoRef} src='/videos/output.mp4' muted playsInline preload='auto' />
+      </div>
     </>
   );
 };
